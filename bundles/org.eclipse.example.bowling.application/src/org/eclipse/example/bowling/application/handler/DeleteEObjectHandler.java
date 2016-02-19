be86d80c3e808cb6f670ba6f4eb7d562e64dac80
Philip Langer <planger@eclipsesource.com>
2016-02-19 14:52:58 +0100

    Use only one delete action instead of multiple ones

--- bundles/org.eclipse.example.bowling.application/src/org/eclipse/example/bowling/application/handler/DeleteEObjectHandler.java
+++ bundles/org.eclipse.example.bowling.application/src/org/eclipse/example/bowling/application/handler/DeleteEObjectHandler.java
@@ -8,23 +8,26 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.command.DeleteCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.example.bowling.Game;
+import org.eclipse.example.bowling.Matchup;
+import org.eclipse.example.bowling.Player;
 
-public class DeleteGameHandler {
+public class DeleteEObjectHandler {
 
 	@Execute
-	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) Game game) {
-		EditingDomain editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(game);
-		Command command = DeleteCommand.create(editingDomain, game);
+	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) EObject eObject) {
+		EditingDomain editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(eObject);
+		Command command = DeleteCommand.create(editingDomain, eObject);
 		editingDomain.getCommandStack().execute(command);
 	}
 
 	@CanExecute
-	public boolean canExecute(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) Game game) {
-		return game != null;
+	public boolean canExecute(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) EObject eObject) {
+		return eObject instanceof Player || eObject instanceof Game || eObject instanceof Matchup;
 	}
 
 }
