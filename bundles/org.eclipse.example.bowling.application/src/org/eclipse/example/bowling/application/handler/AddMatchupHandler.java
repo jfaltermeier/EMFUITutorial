6394b6484180c52b0a3b528d51921ede236ffcd0
Philip Langer <planger@eclipsesource.com>
2016-02-19 12:57:41 +0100

    Switch to literals that are imported with static import

--- bundles/org.eclipse.example.bowling.application/src/org/eclipse/example/bowling/application/handler/AddMatchupHandler.java
+++ bundles/org.eclipse.example.bowling.application/src/org/eclipse/example/bowling/application/handler/AddMatchupHandler.java
@@ -1,27 +1,26 @@
 
 package org.eclipse.example.bowling.application.handler;
 
+import static org.eclipse.example.bowling.BowlingPackage.Literals.TOURNAMENT__MATCHUPS;
+
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.emf.common.command.Command;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.example.bowling.BowlingFactory;
-import org.eclipse.example.bowling.BowlingPackage;
 import org.eclipse.example.bowling.Tournament;
 import org.eclipse.example.bowling.dataservice.BowlingDataService;
 
 public class AddMatchupHandler {
 
 	private static final BowlingFactory BOWLING_FACTORY = BowlingFactory.eINSTANCE;
-	private static final EReference TOURNAMENT_MATCHUPS = BowlingPackage.eINSTANCE.getTournament_Matchups();
 
 	@Execute
 	public void execute(BowlingDataService dataService) {
 		Tournament tournament = dataService.getTournament();
 		EditingDomain editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(tournament);
-		Command command = AddCommand.create(editingDomain, tournament, TOURNAMENT_MATCHUPS,
+		Command command = AddCommand.create(editingDomain, tournament, TOURNAMENT__MATCHUPS,
 				BOWLING_FACTORY.createMatchup());
 		editingDomain.getCommandStack().execute(command);
 
--- bundles/org.eclipse.example.bowling.application/src/org/eclipse/example/bowling/application/handler/AddPlayerHandler.java
+++ bundles/org.eclipse.example.bowling.application/src/org/eclipse/example/bowling/application/handler/AddPlayerHandler.java
@@ -1,28 +1,27 @@
- 
+
 package org.eclipse.example.bowling.application.handler;
+
+import static org.eclipse.example.bowling.BowlingPackage.Literals.LEAGUE__PLAYERS;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.emf.common.command.Command;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.example.bowling.BowlingFactory;
-import org.eclipse.example.bowling.BowlingPackage;
 import org.eclipse.example.bowling.League;
 import org.eclipse.example.bowling.dataservice.BowlingDataService;
 
 public class AddPlayerHandler {
-	
+
 	private static final BowlingFactory BOWLING_FACTORY = BowlingFactory.eINSTANCE;
-	private static final EReference LEAGUE_PLAYERS = BowlingPackage.eINSTANCE.getLeague_Players();
 
 	@Execute
 	public void execute(BowlingDataService dataService) {
 		League leage = dataService.getLeage();
 		EditingDomain editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(leage);
-		Command command = AddCommand.create(editingDomain, leage, LEAGUE_PLAYERS, BOWLING_FACTORY.createPlayer());
+		Command command = AddCommand.create(editingDomain, leage, LEAGUE__PLAYERS, BOWLING_FACTORY.createPlayer());
 		editingDomain.getCommandStack().execute(command);
 	}
-		
+
 }
